6575e6a02881ceb90ce860d1637c11d255424bc7
Jordan Johnson <jmj@fellowhuman.com>
2015-01-03 18:40:32 -0800

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define collection "wam")
+(define scribblings '(("doc.scrbl")))
+;(define compile-omit-paths '("tests.rkt"))
+;(define deps '("base"))
+;(define build-deps '("scribble-lib"))
